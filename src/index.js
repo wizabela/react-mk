c26479f37fbe09dd1d8d6159efeebfe7ed157acf
id github: wizabela <izabela.wyrebska@gmail.com>
2022-03-07 12:03:04 +0100

    add prompt and render name from prompt or strong with comment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = prompt('Podaj imię');
+
+if (name === '') {
+    render(
+        <strong>Brak imienia.</strong>,
+        document.getElementById('root')
+    );
+} else {
+    render(
+        <h1>{name}</h1>,
+        document.getElementById('root')
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
