19129b78777a35f4319efafa2edf540db3ba148b
id github: wizabela <izabela.wyrebska@gmail.com>
2022-03-14 17:08:02 +0100

    wip clock with useEffect

--- src/Clock.js
+++ src/Clock.js
@@ -1,24 +1,41 @@
 import './App.css';
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 
-export class Clock extends Component {
-    state = {
-        date: new Date(),
-    };
-    componentDidMount() {
-        this.timeId = setInterval(() => {
-            this.setState({
-                date: new Date(),
-            })
-        }, 1000)
-    }
-    componentWillUnmount() {
-        clearInterval(this.timeId);
-    }
-    render() {
-        const {date} = this.state;
-        return <p>{date.toLocaleString('pl')}</p>
-    }
-}
+//funkcyjny komponent:
+export const Clock = props => {
+    const [dt, setDt] = useState(new Date());
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setDt(new Date());
+        }, 1000);
+
+    }, []);
+
+    return <p>{dt.toLocaleString()}</p>;
+};
+
+
+//klasowy komponent:
+// export class Clock extends Component {
+//     state = {
+//         date: new Date(),
+//     };
+//     componentDidMount() {
+//         this.timeId = setInterval(() => {
+//             this.setState({
+//                 date: new Date(),
+//             })
+//         }, 1000)
+//     }
+//     componentWillUnmount() {
+//         clearInterval(this.timeId);
+//     }
+//     render() {
+//         const {date} = this.state;
+//         return <p>{date.toLocaleString('pl')}</p>
+//     }
+// }
 
 //Wyświetla aktualną datę i czas w komponencie.
