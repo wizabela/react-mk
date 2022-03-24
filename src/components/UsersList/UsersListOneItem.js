06275e749e4c037222a30cd1d270a983900e0684
id github: wizabela <izabela.wyrebska@gmail.com>
2022-03-24 15:41:35 +0100

    UsersList with avatars exercise

--- src/components/UsersList/UsersListOneItem.js
+++ src/components/UsersList/UsersListOneItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import './UsersListOneItem.css';
+
 export const UsersListOneItem = props => (
-    <li key={props.user.username}>
+    <li key={props.user.username} className="UsersListOneItem">
+        <img
+            className="UsersListOneItem__photo"
+            src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}
+            alt={`Photo of ${props.user.first_name}`}
+        />
         <strong>{props.user.first_name} {props.user.last_name}</strong> {props.user.username}
     </li>
 );
