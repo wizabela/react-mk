5e754bb24318ef68379b2a5d9d5e491dd5ff5229
id github: wizabela <izabela.wyrebska@gmail.com>
2022-03-27 16:16:40 +0200

    UsersList remove items from the list

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {UsersListOneItem} from "./UsersListOneItem";
 
 import './UsersList.css';
 
-export const UsersList = props => (
-    <ul className="UsersList">
-        {
-            [...props.users]
-                .sort((a, b) => a.username.localeCompare(b.username))
-                .map(userObj => <UsersListOneItem
-                    user={userObj}
-                    key={userObj.username}
-                />)
-        }
-    </ul>
-);
+export const UsersList = props => {
+    const [list, setList] = useState(props.users);
+
+    const removeItem = (username) => {
+        setList(list => (
+            list
+                .filter(one => one.username !== username)
+        ));
+    };
+
+    return (
+        <ul className="UsersList">
+            {
+                [...list]
+                    .sort((a, b) => a.username.localeCompare(b.username))
+                    .map(userObj => <UsersListOneItem
+                        user={userObj}
+                        key={userObj.username}
+                        onRemoveItem={removeItem}
+                    />)
+            }
+        </ul>
+    );
+};
--- src/components/UsersList/UsersListOneItem.js
+++ src/components/UsersList/UsersListOneItem.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 import './UsersListOneItem.css';
 
-export const UsersListOneItem = props => (
-    <li key={props.user.username} className="UsersListOneItem">
-        <img
-            className="UsersListOneItem__photo"
-            src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}
-            alt={`Photo of ${props.user.first_name}`}
-        />
-        <div  className="UsersListOneItem__text">
-            <p>
-                {props.user.first_name} {props.user.last_name}
-            </p>
-            <p className="UsersListOneItem__username">
-                {props.user.username}
-            </p>
-        </div>
-    </li>
-);
+export const UsersListOneItem = props => {
+    const personLiClicked = () => {
+        props.onRemoveItem(props.user.username);
+    };
+
+    return (
+        <li key={props.user.username}
+            className="UsersListOneItem"
+            onClick={personLiClicked}>
+            <img
+                className="UsersListOneItem__photo"
+                src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}
+                alt={`Photo of ${props.user.first_name}`}
+            />
+            <div className="UsersListOneItem__text">
+                <p>
+                    {props.user.first_name} {props.user.last_name}
+                </p>
+                <p className="UsersListOneItem__username">
+                    {props.user.username}
+                </p>
+            </div>
+        </li>
+    );
+};
