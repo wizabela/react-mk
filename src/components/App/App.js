5896ff6ae3523626c7765fe61ec23ebaa4062915
id github: wizabela <izabela.wyrebska@gmail.com>
2022-03-28 14:32:56 +0200

    form exercise

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,55 @@
-import React from "react";
-import {ColorBox} from "../ColorBox/ColorBox";
+import React, {useState} from "react";
 
 import './App.css';
 
 
 export const App = props => {
-return <ColorBox/>;
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [age, setAge] = useState(0);
+
+    const sendForm = e => {
+        e.preventDefault();
+
+        const person = {
+            firstName,
+            lastName,
+            age,
+        };
+        console.log(person);
+    };
+
+    return <form onSubmit={sendForm}>
+        <p>
+            <label>
+                Imię:<br/>
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                />
+            </label>
+        </p>
+        <p>
+            <label>
+                Nazwisko:<br/>
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}/>
+            </label>
+        </p>
+        <p>
+            <label>
+                Wiek:<br/>
+                <input
+                    type="number"
+                    value={age}
+                    onChange={e => setAge(Number(e.target.value))}/>
+            </label>
+        </p>
+        <p>
+            <button type="submit">Zarejestruj</button>
+        </p>
+    </form>
 };
